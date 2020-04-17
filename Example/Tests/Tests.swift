8b8425e74f3ae2d692505d6b49e00c67f8880fe5
Zacharysp <dongjiezach@gmail.com>
2020-04-17 14:58:38 -0700

    feat: add scroll function (#10)
    remove auto scroll after build calendar.
    add protocol function notifies after build calendar.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,27 +1,25 @@
 import XCTest
 @testable import CalendarHeatmap
 
-class Tests: XCTestCase {
+class TestCalendarData: XCTestCase {
     
-    private let startDate = Date(2020, 3, 1)
-    private let endDate = Date(2020, 4, 10)
+    private let startDate = Date.dateOf(2020, 3, 1)!
+    private let endDate = Date.dateOf(2020, 4, 10)!
     private lazy var heatmapData: CalendarHeatmapData = {
         return CalendarHeatmapData(config: CalendarHeatmapConfig(), startDate: startDate, endDate: endDate)
     } ()
     
-    func testCalendarSectionCount() {
-        XCTAssertEqual(heatmapData.sectionCount, 2)
+    func testCalendarDayCount() {
+        XCTAssertEqual(heatmapData.daysCount, 41)
     }
     
-    func testCalendarItemCount() {
-        XCTAssertEqual(heatmapData.itemCountIn(section: 0), 35)
-        XCTAssertEqual(heatmapData.itemCountIn(section: 1), 6)
+    func testCalendarDataHeader() {
+        XCTAssertEqual(heatmapData.headerData.first!.month, 3)
+        XCTAssertEqual(heatmapData.headerData.last!.month, 4)
     }
     
     func testCalendarDataItem() {
-        let firstDate = Date(2020, 3, 1)
-        XCTAssertEqual(heatmapData.itemAt(indexPath: IndexPath(item: 0, section: 0)), firstDate)
-        let lastDate = Date(2020, 4, 10)
-        XCTAssertEqual(heatmapData.itemAt(indexPath: IndexPath(item: 5, section: 1)), lastDate)
+        XCTAssertEqual(heatmapData.itemAt(indexPath: IndexPath(item: 0, section: 0)), startDate)
+        XCTAssertEqual(heatmapData.itemAt(indexPath: IndexPath(item: 40, section: 0)), endDate)
     }
 }
